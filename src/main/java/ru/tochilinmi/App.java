64206d37044e0c21163ee620b41c5f8bdabf1cc5
TochilinMI <tochilin.m.i@mail.ru>
2022-09-12 21:48:43 +0300

    Собрал телеграмм бота и настроил к нему подключение, разобрался с локализацией
    Бот начинает диалог с пользователем, создаёт его сущность, пока в массив, если пользователя нет в системе, то просит авторизоваться,
    иначе просит прислать фото, текст и геточку.
    После сохраняет изображение в папку.

--- src/main/java/ru/tochilinmi/App.java
+++ src/main/java/ru/tochilinmi/App.java
@@ -1,7 +1,73 @@
 package ru.tochilinmi;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.request.GetFile;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.GetFileResponse;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class App{
-	public final static void main(String[] args){
-		System.out.println("Hello Java!");
+	public static void main(String[] args) throws IOException {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream("app.properties"));
+//		String TOKEN = new TelegramBot();
+
+		//пока вместо базы
+		Map<Long, User> users = new HashMap<>();
+
+		TelegramBot bot = new TelegramBot(properties.getProperty("telegram_token"));
+
+		bot.setUpdatesListener(updates -> {
+			updates.forEach(System.out::println);
+
+			updates.forEach(update -> {
+//				Integer userId = update.message().from().id();
+				// В лонг так как некоторые id не помещаются в int, на php проверил
+				Long userId = update.message().from().id();
+				// проверка наличия логина и пароля
+				if (!users.containsKey(userId)){
+					bot.execute(new SendMessage(
+							update.message().chat().id(),
+							"Вам необходимо прислать логин и пароль в одном предложении через пробел"
+					));
+					users.put(userId, null);
+				} else if(users.get(userId) == null){ // Запись логина и пароля
+					String[] loginAndPassword = update.message().text().split(" ");
+					User user = new User(loginAndPassword[0], loginAndPassword[1]);
+					users.put(userId, user);
+					bot.execute(new SendMessage(
+							update.message().chat().id(),
+							"Всё работает! \n Теперь Вы можете присылать нам текст/изображение/геопозицию для"
+									+ "Инстаграм (в одном сообщении)"
+					));
+				} else {
+					System.out.println(update.toString());
+					Post post = new Post();
+					post.setTitle(update.message().text());
+					GetFileResponse fileResponse = bot.execute( new GetFile(update.message().photo()[0].fileId()));
+					String fullPath = bot.getFullFilePath(fileResponse.file());
+					System.out.println(fullPath);
+					try{
+						HttpDownload.downloadFile(fullPath,"./images",update.message().photo()[0].fileId()+".jpg");
+					} catch(IOException e){
+						System.err.println(e.getMessage());
+					}
+					post.setPhoto(new File("./images/"+update.message().photo()[0].fileId()+".jpg").getPath());
+					users.get(userId).addPost(post);
+
+					System.out.println(users.toString());
+				}
+			});
+
+			return UpdatesListener.CONFIRMED_UPDATES_ALL;
+		});
 	}
 }
---
+++ src/main/java/ru/tochilinmi/Post.java
@@ -0,0 +1,32 @@
+package ru.tochilinmi;
+
+public class Post{
+    private String title;
+    private String photo;
+    private String geo;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getGeo() {
+        return geo;
+    }
+
+    public void setGeo(String geo) {
+        this.geo = geo;
+    }
+}
+
